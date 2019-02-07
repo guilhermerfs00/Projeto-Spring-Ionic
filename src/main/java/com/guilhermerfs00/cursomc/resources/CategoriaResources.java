4aed69c29d5d49aa951a802ce162a962a2cfd01d
guilhermerfs00 <guilherme.rfs00@gmail.com>
2019-02-06 22:00:23 -0200

    Testando a primeira classe de dominio

--- src/main/java/com/guilhermerfs00/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/guilhermerfs00/cursomc/resources/CategoriaResources.java
@@ -1,15 +1,27 @@
 package com.guilhermerfs00.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.guilhermerfs00.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {	
-		return "REST está funcionando";
+	public List<Categoria> listar() {	
+		
+		Categoria cat1 = new Categoria(1,"informatica");
+		Categoria cat2 = new Categoria(2,"escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
