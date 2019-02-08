6e0dcab59a9619e828624c44d1b9a0b24b01c6ea
guilhermerfs00 <guilherme.rfs00@gmail.com>
2019-02-08 08:55:53 -0200

    Criando repository e service para Categoria

---
+++ src/main/java/com/guilhermerfs00/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.guilhermerfs00.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guilhermerfs00.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
