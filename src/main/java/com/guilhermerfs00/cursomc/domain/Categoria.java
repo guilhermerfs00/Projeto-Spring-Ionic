9dced383bb4a2a9ca4881dc551882c8694f63382
guilhermerfs00 <guilherme.rfs00@gmail.com>
2019-02-08 12:03:34 -0200

    Ajustes no andpoint/categorias/id

--- src/main/java/com/guilhermerfs00/cursomc/domain/Categoria.java
+++ src/main/java/com/guilhermerfs00/cursomc/domain/Categoria.java
@@ -1,35 +1,38 @@
 package com.guilhermerfs00.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
-public class Categoria implements Serializable{
-	private static final long serialVersionUID = 1L;
-	
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = -963796407803234179L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String name;
+
+	@JsonManagedReference
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<Produto>();
 
 	public Categoria() {
 	}
 
-	public Categoria(int id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
-
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -41,11 +44,25 @@ public class Categoria implements Serializable{
 		this.name = name;
 	}
 
+	public Categoria(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + id;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -58,7 +75,10 @@ public class Categoria implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		if (id != other.id)
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
--- src/main/java/com/guilhermerfs00/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/guilhermerfs00/cursomc/resources/CategoriaResources.java
@@ -25,3 +25,4 @@ public class CategoriaResources {
 		return ResponseEntity.ok().body(obj);
 	}
 }
+
