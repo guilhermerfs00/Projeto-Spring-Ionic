cf80cf5bfd1c717744e31010fe4620bc0f932e1e
guilhermerfs00 <guilherme.rfs00@gmail.com>
2019-02-12 15:58:34 -0200

    Paginacao com parametros

--- src/main/java/com/guilhermerfs00/cursomc/CursomcApplication.java
+++ src/main/java/com/guilhermerfs00/cursomc/CursomcApplication.java
@@ -72,6 +72,13 @@ public class CursomcApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama");
+		Categoria cat4 = new Categoria(null, "Eletônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Móvel");
+		Categoria cat8 = new Categoria(null, "Perfumaria");
+		Categoria cat9 = new Categoria(null, "Quarto");
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -84,7 +91,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8,cat9));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
