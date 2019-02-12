e1a5317ce4a0281fde3fc507c462c4053952cf84
guilhermerfs00 <guilherme.rfs00@gmail.com>
2019-02-12 08:36:27 -0200

    Endpoint / clientes/{id} disponivel

--- src/main/java/com/guilhermerfs00/cursomc/domain/Cliente.java
+++ src/main/java/com/guilhermerfs00/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.guilhermerfs00.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -28,8 +29,9 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
-	private List <Endereco> endereco =new ArrayList<>();
+	private List <Endereco> endereco = new ArrayList<>();
 	
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
