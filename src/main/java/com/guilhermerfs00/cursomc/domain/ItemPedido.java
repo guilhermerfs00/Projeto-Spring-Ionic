b0f3993c1c0c37c2894757e216d9215d94854e51
guilhermerfs00 <guilherme.rfs00@gmail.com>
2019-02-14 10:57:29 -0200

    Inserindo Pedido

--- src/main/java/com/guilhermerfs00/cursomc/domain/ItemPedido.java
+++ src/main/java/com/guilhermerfs00/cursomc/domain/ItemPedido.java
@@ -19,31 +19,37 @@ public class ItemPedido implements Serializable {
 	private Integer quantidade;
 	private Double preco;
 	
-	public ItemPedido(){	
+	public ItemPedido() {
 	}
 
-	public ItemPedido(Pedido pedido, Produto produto,Double desconto ,Integer quantidade, Double preco) {
+	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
 		super();
 		id.setPedido(pedido);
 		id.setProduto(produto);
-		this.desconto= desconto;
+		this.desconto = desconto;
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
 
-	
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
 	
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
 	
-	public double getSubTotal() {
-		return (preco - desconto) * quantidade;
-	} 
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
 	
 	public Produto getProduto() {
 		return id.getProduto();
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
 	}
 	
 	public ItemPedidoPK getId() {
--- src/main/java/com/guilhermerfs00/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/guilhermerfs00/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.guilhermerfs00.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.guilhermerfs00.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
